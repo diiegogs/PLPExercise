67f13859ac8e01e4da33bebfdb4bb3fbbd9adbaf
JDiego-Garcia <diiegogarcs26@gmail.com>
2025-05-30 13:40:11 -0600

    Initial Commit

---
+++ PLPExercise/PLPExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLPExerciseApp.swift
+//  PLPExercise
+//
+//  Created by JDiego Garcia on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PLPExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
